e25503186773bfebbee62ec9705ecf34032ddd5a
Joseandres <joseandresmontesino@gmail.com>
2024-01-16 16:55:31 -0500

    post axios option 1

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-function getUser(){
+function main(){
 
     //Option 1 
     // axios.get("https://jsonplaceholder.typicode.com/users")
 
 
     //Option 2
-    axios.get("https://jsonplaceholder.typicode.com/users?",{
-        params: {
-            _limit:1
+    // axios.get("https://jsonplaceholder.typicode.com/users?",{
+    //     params: {
+    //         _limit:1
+    //     }
+    // })
+    // .then((res) => {
+    //     console.log(res.data)
+    // })
+    //   .catch((err)=>console.log(err))
+
+
+    axios({
+        method: "POST",
+        url: "https://reqres.in/api/register",
+        data:{
+            email: "eve.holt@reqres.in",
+            password: "pistol"
         }
     })
-    .then((res) => {
-        console.log(res.data)
-    })
-      .catch((err)=>console.log(err))
+      .then((res) => console.log(res))
+      .catch((err)=> console.log(err))
 }
 
 
-getUser()
+main()
