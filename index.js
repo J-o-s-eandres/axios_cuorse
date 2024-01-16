e1117d6007c27c81da5ec2edba9bbfd28b22ab8e
Joseandres <joseandresmontesino@gmail.com>
2024-01-16 16:43:40 -0500

    get data and limit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getUser(){
 
+    //Option 1 
+    // axios.get("https://jsonplaceholder.typicode.com/users")
+
+
+    //Option 2
+    axios({
+        url:"https://jsonplaceholder.typicode.com/users?",
+        method:"GET",
+        params: {
+            _limit:3
+        }
+    }).then((res) => {
+        console.log(res.data)
+    })
+      .catch((err)=>console.log(err))
+}
+
+
+getUser()
